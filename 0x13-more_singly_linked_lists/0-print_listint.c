3960e1b7317e43ac939cfeaa7f3bc12eaa8fe8cf
RefilweNkaunyane <refilwe.nkaunyane@gmail.com>
2023-04-04 01:26:53 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,4 @@
+
 #include "lists.h"
 
 /**
@@ -11,11 +12,11 @@ size_t print_listint(const listint_t *h)
 	size_t num = 0;
 
 	while (h)
-	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
-	}
+{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+}
 
-	return (num);
+return (num);
 }
